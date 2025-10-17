6e4927c29353c4483e558e36b41c99ee44815bd6
Andrew Herr <andrew.herr@testdouble.com>
2025-10-17 10:03:26 -0600

    bump: adopt rails 6.0 defaults
    including changes that do not work with split deployment (in case
    there are multiple copies of the app deployed :))

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Eval360
   class Application < Rails::Application
-    config.load_defaults 5.1
-
-    config.autoloader = :zeitwerk
+    config.load_defaults 6.0
 
     config.generators do |g|
       g.view_specs false
