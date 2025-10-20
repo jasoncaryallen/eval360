b6dbc2866cd5f74f38443299b861c28ce5417ce3
Jason Allen <jasoncaryallen@gmail.com>
2025-10-20 15:42:32 -0400

    feat: app:update application.rb conflicts resolved

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,13 @@ module Eval360
     config.active_record.belongs_to_required_by_default = false
 
     config.x.loi = "YearlongIndividual"
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
